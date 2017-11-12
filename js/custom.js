c9d96822260b53668b92985556c50c3e4937fa92
Konstantin Kosenkov <firebbs@ya.ru>
2017-11-12 16:00:00 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	$('.resultlist-filters ul select,.Dataface_QuickForm-widget-cell input,.Dataface_QuickForm-widget-cell select').addClass('form-control').css({
+		width: 'auto',
+		display: 'inline',
+		'margin-left':'5px'
+	});
+});
